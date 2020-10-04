7a7645064900e058ae238b5cfbc0d98d92dfe19a
digantshrestha <digantshrestha@gmail.com>
2020-10-04 11:10:43 +0545

    bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Session Test</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
 </head>
 <body>
     <div class="page-header">
