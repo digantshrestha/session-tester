73bcfc4cbe6ecde3d7b303074e8072387f602d84
digantshrestha <digantshrestha@gmail.com>
2020-10-04 11:05:59 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session Test</title>
+</head>
+<body>
+    <div class="page-header">
+        <div class="container">
+            <h1>Session Tester</h1>
+        </div>
+    </div>
+</body>
+</html>
